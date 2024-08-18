223ef98ae0044e669078c8e80f20cbfe3230d85f
ayu10293 <snof11513@gmail.com>
2024-08-18 14:37:27 +0900

    third commit.

---
+++ src/a/Foo.java
@@ -0,0 +1,9 @@
+package a;
+import java.time.LocalDate;
+
+class Foo {
+	public void showDate() {
+		System.out.println(LocalDate.now());
+	}
+	
+}
---
+++ src/b/Foomain.java
@@ -0,0 +1,9 @@
+package b;
+
+public class Foomain {
+
+	public static void main(String[] args) {
+		new Foo().showDate();
+	}
+
+}
