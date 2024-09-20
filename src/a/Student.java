61fb39fff866c9c5d02baac632d7a13dea01c7df
ayu10293 <snof11513@gmail.com>
2024-09-21 02:06:38 +0900

    せかチャン

--- src/a/Student.java
+++ src/a/Student.java
@@ -1,5 +1,20 @@
 package a;
 
 public class Student {
-
+	String name;
+	int mathScore ;
+	int engScore ;
+	
+	void display() {
+		System.out.println(name + "さん");
+		System.out.println("英語" + engScore + "点" + "" + "数学" + mathScore + "点");
+	}
+	void setScore (int math , int eng) {
+		mathScore = math;
+		engScore = eng;
+	}
+	double getAvg() {
+		double avg = mathScore + engScore / 2.0;
+		return avg;
+	}
 }
