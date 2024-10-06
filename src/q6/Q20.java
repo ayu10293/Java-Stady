7b7755e1d88bbbb198b92d0bcf10038810e434ff
ayu10293 <snof11513@gmail.com>
2024-10-06 22:11:33 +0900

    T

---
+++ src/q6/Q20.java
@@ -0,0 +1,10 @@
+package q6;
+import java.util.*;
+
+public class Q20 {
+	<T> Collection<T> foo(Collection c){ return null;}
+	<T> List<T> foo(List c){ return null;}
+	<T> List<T> foo(Collection c){ return null;}
+	<T> Collection<T> foo(Map c){ return null;}
+	}
+
