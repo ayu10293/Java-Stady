4b44045f92b4d8f8f59e15c75997a929b692d66e
ayu10293 <snof11513@gmail.com>
2024-10-18 22:43:01 +0900

    まだ理解できてない

--- src/Main10.java
+++ src/Main10.java
@@ -4,10 +4,21 @@ import java.util.List;
 
 public class Main10 {
 	public static void main(String[] args) {
-		List<integer>data =
+		List<Integer>data =
 				new ArrayList<>(Arrays.asList(1,2,3,4,5));
-		data.removeif((Integer i) -> {return i % 2 != 0;} );
+		data.removeIf((Integer i) -> {return i % 2 != 0;} );
 		System.out.println(data);
+		
+//		List<String> words = Arrays.asList("Tanaka", "Sato");
+//		words.replaceAll((String str) -> {return str.toUpperCase();});
+//		System.out.println(words);
+//		
+//		List<Integer> list = Arrays.asList(20, 30, 10);
+//		list.sort((Integer o1, Integer o2) -> {return o2.compareTo(o1);});
+//		System.out.println(list);
+//		
+//		List<Integer>lists = Arrays.asList(20,30,10);
+//		lists.forEach((Integer num) -> { System.out.println(num + " ");});
 	}
 
 }
