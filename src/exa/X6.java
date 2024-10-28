ee04848b862d803ba8d4f5ef0d6f89beff41828c
ayu10293 <snof11513@gmail.com>
2024-10-28 19:56:35 +0900

    uncheckedの問題

---
+++ src/exa/X6.java
@@ -0,0 +1,23 @@
+package exa;//7-11
+
+public class X6 {
+	static void method() {
+		int ary [] = new int [5];
+		for (int i = 0; i <= 5; i++) ary [i] = i;
+	}
+
+	public static void main(String[] args) {
+		try {
+			method();
+			System.out.print("try ");
+		}catch(ArrayIndexOutOfBoundsException e) {
+			System.out.print("catch1 ");
+		}catch(Exception e) {
+			System.out.print("catch2 ");
+		}finally {
+			System.out.print("finally ");
+		}
+		System.out.print("done. ");
+	}
+
+}
